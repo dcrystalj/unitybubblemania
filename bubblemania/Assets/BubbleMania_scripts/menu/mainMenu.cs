7750648e14ebaebf9e29bd9b285d82768734ce2b
dcrystalj <dcrystalj@gmail.com>
2013-01-10 19:21:14 +0100

    fixed mainMenu scene

--- bubblemania/Assets/BubbleMania_scripts/menu/mainMenu.cs
+++ bubblemania/Assets/BubbleMania_scripts/menu/mainMenu.cs
@@ -5,7 +5,6 @@ public class mainMenu : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
@@ -13,9 +12,17 @@ public class mainMenu : MonoBehaviour {
 
 	}
 	void OnGUI(){
-		if(GUI.Button(new Rect(0,0,200,100),"Start Game")){
+		if(GUI.Button(new Rect(Screen.width/2-50,Screen.height/2-50,100,50),"Start Game")){
 			Application.LoadLevel("unity");
 		}
+		if(GUI.Button(new Rect(Screen.width/2-50,Screen.height/2,100,50),"Quit")){
+			Application.Quit();
+		}
+		GUI.color = Color.green;
+		GUI.Label(new Rect(Screen.width-100,Screen.height-60,280,280),"CopyRights to:");
+		GUI.color = Color.red;
+		GUI.Label(new Rect(Screen.width-100,Screen.height-40,250,250),"Gasper Urh");
+		GUI.Label(new Rect(Screen.width-100,Screen.height-20,270,270),"Tomaz Tomazic");
 	}
 
 }
