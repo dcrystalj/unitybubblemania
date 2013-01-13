a6deb9edf82267904be3947a121298cdebb126e9
urgas9 <gasper.urh@gmail.com>
2013-01-13 09:32:23 +0100

    Constant velocity of bubbles, added colored start point

--- bubblemania/Assets/BubbleMania_scripts/bubbleMove.cs
+++ bubblemania/Assets/BubbleMania_scripts/bubbleMove.cs
@@ -2,19 +2,11 @@ using UnityEngine;
 using System.Collections;
 
 public class bubbleMove : MonoBehaviour {
-	public bool UpdatePosition;
-	public int timeOnPath=20;
-    public float DelayUpdatePos;
-    public float FirstYPos;
-    public float MovementSpeed;
+	public int hitrost=20;
 	void Start () {
-       UpdatePosition = true;
-       DelayUpdatePos = 1;
-       FirstYPos = 5;
-       MovementSpeed = 5;
 
-      //Here i'm telling to the owner of this script to wall by the path created with the name "EnemyPath1" 
-		iTween.MoveTo(this.gameObject,iTween.Hash("path",iTweenPath.GetPath("bubblesPath"),"speed",timeOnPath));
+      //Vsak objekt, ki mu pripada ta skripta damo na iTween pot.
+		iTween.MoveTo(this.gameObject,iTween.Hash("path",iTweenPath.GetPath("bubblesPath"),"speed",hitrost, "easetype", "linear"));
     }
 	
 	 void Update () {
